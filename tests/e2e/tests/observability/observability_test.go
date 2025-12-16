fac90ffa9a8707ca26f7975fbfa9c0513e25d518
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2025-12-16 21:01:15 +0100

    VS in progress

--- tests/e2e/tests/observability/observability_test.go
+++ tests/e2e/tests/observability/observability_test.go
@@ -3,8 +3,11 @@ package observability
 import (
 	_ "embed"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/virtual_service"
 
 	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/namespace"
 
@@ -42,34 +45,11 @@ func TestObservability(t *testing.T) {
 		err = extauth.CreateHTTPGateway(t)
 		require.NoError(t, err)
 
-		//vs := &v1alpha3.VirtualService{
-		//	ObjectMeta: metav1.ObjectMeta{
-		//		Name:      "httpbin",
-		//		Namespace: "istio-system",
-		//	},
-		//	Spec: alpha3.VirtualService{
-		//		Gateways: []string{"kyma-system/kyma-gateway"},
-		//		Hosts:    []string{"*"},
-		//		Http: []*alpha3.HTTPRoute{
-		//			{
-		//				Name: "httpbin",
-		//				Match: []*alpha3.HTTPMatchRequest{
-		//					{
-		//						Uri: &alpha3.StringMatch{
-		//							MatchType: &alpha3.StringMatch_Exact{
-		//								Exact: "/",
-		//							},
-		//						},
-		//					},
-		//				},
-		//			},
-		//		},
-		//	},
-		//}
-		//err = cc.Create(t.Context(), vs)
-		//require.NoError(t, err)
-		//println("TERAZ IDZ SPRAWDZ")
-		//time.Sleep(100 * time.Second)
+		//create vs
+		err = virtual_service.CreateVirtualService(t, "httpbin", "default")
+		require.NoError(t, err)
 
+		println("TERAZ IDZ I SPRAWDZ")
+		time.Sleep(100 * time.Second)
 	})
 }
