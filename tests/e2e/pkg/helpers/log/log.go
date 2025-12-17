fca0486c2728c4c0c5295ee7b24897d36efe8348
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2025-12-17 22:11:17 +0100

    First part of observability works

--- tests/e2e/pkg/helpers/log/log.go
+++ tests/e2e/pkg/helpers/log/log.go
@@ -2,8 +2,13 @@ package log
 
 import (
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/client"
 )
 
 func StructToPrettyJson(t *testing.T, v interface{}) string {
@@ -11,4 +16,26 @@ func StructToPrettyJson(t *testing.T, v interface{}) string {
 	str, err := json.MarshalIndent(v, "", "    ")
 	assert.NoError(t, err)
 	return string(str)
+}
+
+func GetLogsFromIstioProxy(t *testing.T, podName, podNamespace string) ([]byte, error) {
+	t.Helper()
+	config := client.GetKubeConfig(t)
+	k8sClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		t.Logf("Failed to create k8s client: %v", err)
+		return nil, err
+	}
+
+	req := k8sClient.CoreV1().Pods(podNamespace).GetLogs(podName, &v1.PodLogOptions{
+		Container: "istio-proxy",
+	})
+
+	logs, err := req.DoRaw(t.Context())
+	if err != nil {
+		t.Logf("Failed to get logs from istio-proxy container: %v", err)
+		return nil, err
+	}
+
+	return logs, nil
 }
--- tests/e2e/tests/observability/observability_test.go
+++ tests/e2e/tests/observability/observability_test.go
@@ -1,11 +1,21 @@
 package observability
 
 import (
+	"context"
 	_ "embed"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/e2e-framework/klient/decoder"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/log"
+
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/client"
+	httphelper "github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/http"
 
 	infrahelpers "github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/infrastructure"
 
@@ -26,8 +36,10 @@ var IstioCR string
 var VirtualService string
 
 func TestObservability(t *testing.T) {
-
 	t.Run("Logs from stdout-json envoyFileAccessLog provider are in correct format", func(t *testing.T) {
+		c, err := client.ResourcesClient(t)
+		require.NoError(t, err)
+
 		require.NoError(
 			t,
 			modulehelpers.CreateIstioOperatorCR(t,
@@ -35,7 +47,7 @@ func TestObservability(t *testing.T) {
 			),
 		)
 
-		err := telemetry.EnableLogs(t)
+		err = telemetry.EnableLogs(t)
 		require.NoError(t, err)
 
 		err = namespace.LabelNamespaceWithIstioInjection(t, "default")
@@ -53,17 +65,70 @@ func TestObservability(t *testing.T) {
 			t,
 			VirtualService,
 			map[string]any{
-				"Name":            "BOGDAN",
-				"HostName":        "*",
+				"Name":            "httpbin",
+				"HostName":        "httpbin.local.kyma.dev",
 				"DestinationHost": httpbinSvcName,
 				"DestinationPort": httpbinPort,
 				"GatewayName":     "kyma-system/kyma-gateway",
 			},
+			decoder.MutateNamespace("default"),
 		)
 		require.NoError(t, err, "Failed to create VirtualService resource")
 		require.NotEmpty(t, createdVS, "Created VirtualService resource should not be empty")
 
-		println("TERAZ IDZ I SPRAWDZ")
-		time.Sleep(100 * time.Second)
+		err = wait.For(func(ctx context.Context) (done bool, err error) {
+			t.Logf("Waiting for endpoint to return 200 OK")
+
+			httpClient := httphelper.NewHTTPClient(t, httphelper.WithPrefix("observability-test"))
+			_, err = httpClient.Get("http://httpbin.local.kyma.dev/headers")
+
+			if err != nil {
+				return false, nil
+			}
+			return true, nil
+
+		}, wait.WithTimeout(10*time.Second), wait.WithInterval(1*time.Second))
+
+		httpbinPods := v1.PodList{}
+		err = c.List(t.Context(), &httpbinPods, resources.WithLabelSelector("app=httpbin"))
+		require.NoError(t, err)
+
+		requiredLogEntries := []string{
+			"start_time",
+			"method",
+			"path",
+			"protocol",
+			"response_code",
+			"response_flags",
+			"response_code_details",
+			"connection_termination_details",
+			"upstream_transport_failure_reason",
+			"bytes_received",
+			"bytes_sent",
+			"duration",
+			"upstream_service_time",
+			"x_forwarded_for",
+			"user_agent",
+			"request_id",
+			"authority",
+			"upstream_host",
+			"upstream_cluster",
+			"upstream_local_address",
+			"downstream_local_address",
+			"downstream_remote_address",
+			"requested_server_name",
+			"route_name",
+			"traceparent",
+			"tracestate",
+		}
+
+		for _, pod := range httpbinPods.Items {
+			logs, err := log.GetLogsFromIstioProxy(t, pod.Name, pod.Namespace)
+			require.NoError(t, err)
+			for _, entry := range requiredLogEntries {
+				require.Containsf(t, string(logs), entry, "Log entry %s not found in logs from pod %s", entry, pod.Name)
+			}
+		}
+
 	})
 }
