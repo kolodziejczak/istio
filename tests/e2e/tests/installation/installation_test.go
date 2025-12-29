3c78d56b879ae0c858ce44f6654b14ec934a7e40
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2025-12-30 00:12:00 +0100

    savepoint before crd ensure

--- tests/e2e/tests/installation/installation_test.go
+++ tests/e2e/tests/installation/installation_test.go
@@ -2,9 +2,20 @@ package installation
 
 import (
 	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gopkg.in/inf.v0"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/client"
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/httpbin"
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/namespace"
 
 	modulehelpers "github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/modules"
 )
@@ -12,8 +23,14 @@ import (
 //go:embed istio_cr_default.yaml
 var IstioDefault string
 
+//go:embed istio_cr_custom_resources.yaml
+var IstioCustomResources string
+
 func TestInstallation(t *testing.T) {
 	t.Run("Installation of Istio module with default values", func(t *testing.T) {
+		c, err := client.ResourcesClient(t)
+		require.NoError(t, err)
+
 		require.NoError(
 			t,
 			modulehelpers.CreateIstioOperatorCR(t,
@@ -21,5 +38,130 @@ func TestInstallation(t *testing.T) {
 			),
 		)
 
+		err = namespace.LabelNamespaceWithIstioInjection(t, "default")
+		require.NoError(t, err)
+
+		_, _, err = httpbin.DeployHttpbin(t, "default")
+		require.NoError(t, err)
+
+		// user workload
+		httpbinPodList := &v1.PodList{}
+		err = c.List(t.Context(), httpbinPodList, resources.WithLabelSelector("app=httpbin"))
+		require.NoError(t, err)
+
+		for _, pod := range httpbinPodList.Items {
+			proxy := pod.Spec.InitContainers[1]
+			require.Equal(t, "istio-proxy", proxy.Name)
+
+			err = assertResources(resourceStruct{
+				Cpu:    *proxy.Resources.Requests.Cpu(),
+				Memory: *proxy.Resources.Requests.Memory(),
+			}, "10m", "192Mi")
+			require.NoError(t, err)
+
+			err = assertResources(resourceStruct{
+				Cpu:    *proxy.Resources.Limits.Cpu(),
+				Memory: *proxy.Resources.Limits.Memory(),
+			}, "1000m", "1024Mi")
+			require.NoError(t, err)
+		}
+
+		// istio-ingressgateway
+		ingressPodList := &v1.PodList{}
+		err = c.List(t.Context(), ingressPodList, resources.WithLabelSelector("app=istio-ingressgateway"))
+		require.NoError(t, err)
+
+		for _, pod := range ingressPodList.Items {
+			proxy := pod.Spec.Containers[0]
+			require.Equal(t, "istio-proxy", proxy.Name)
+			err = assertResources(resourceStruct{
+				Cpu:    *proxy.Resources.Requests.Cpu(),
+				Memory: *proxy.Resources.Requests.Memory(),
+			}, "100m", "128Mi")
+			require.NoError(t, err)
+
+			err = assertResources(resourceStruct{
+				Cpu:    *proxy.Resources.Limits.Cpu(),
+				Memory: *proxy.Resources.Limits.Memory(),
+			}, "2000m", "1024Mi")
+			require.NoError(t, err)
+		}
+
+		// istiod
+		istiodPodList := &v1.PodList{}
+		err = c.List(t.Context(), istiodPodList, resources.WithLabelSelector("app=istiod"))
+		require.NoError(t, err)
+		for _, pod := range istiodPodList.Items {
+			istiod := pod.Spec.Containers[0]
+			require.Equal(t, "discovery", istiod.Name)
+			err = assertResources(resourceStruct{
+				Cpu:    *istiod.Resources.Requests.Cpu(),
+				Memory: *istiod.Resources.Requests.Memory(),
+			}, "100m", "512Mi")
+			require.NoError(t, err)
+
+			err = assertResources(resourceStruct{
+				Cpu:    *istiod.Resources.Limits.Cpu(),
+				Memory: *istiod.Resources.Limits.Memory(),
+			}, "4000m", "2048Mi")
+			require.NoError(t, err)
+		}
 	})
+	t.Run("Installation of Istio module with custom values", func(t *testing.T) {
+		//c, err := client.ResourcesClient(t)
+		//require.NoError(t, err)
+
+		require.NoError(
+			t,
+			modulehelpers.CreateIstioOperatorCR(t,
+				modulehelpers.WithIstioOperatorTemplate(IstioCustomResources),
+				modulehelpers.WithIstioOperatorTemplateValues(map[string]interface{}{
+					"PilotCPULimit":        "1200m",
+					"PilotMemoryLimit":     "1200Mi",
+					"PilotCPURequests":     "15m",
+					"PilotMemoryRequests":  "200Mi",
+					"IGCPULimit":           "1500m",
+					"IGMemoryLimit":        "1200Mi",
+					"IGCPURequests":        "80m",
+					"IGMemoryRequests":     "200Mi",
+					"EgressGatewayEnabled": "true",
+					"EGCPULimit":           "1400m",
+					"EGMemoryLimit":        "1100Mi",
+					"EGCPURequests":        "70m",
+					"EGMemoryRequests":     "190Mi",
+				}),
+			),
+		)
+
+		// check if CRDS are in the cluster
+
+	})
+}
+
+type resourceStruct struct {
+	Cpu    resource.Quantity
+	Memory resource.Quantity
+}
+
+func assertResources(actualResources resourceStruct, expectedCpu, expectedMemory string) error {
+
+	cpuMilli, err := strconv.Atoi(strings.TrimSuffix(expectedCpu, "m"))
+	if err != nil {
+		return err
+	}
+
+	memMilli, err := strconv.Atoi(strings.TrimSuffix(expectedMemory, "Mi"))
+	if err != nil {
+		return err
+	}
+
+	if resource.NewDecimalQuantity(*inf.NewDec(int64(cpuMilli), inf.Scale(resource.Milli)), resource.DecimalSI).Equal(actualResources.Cpu) {
+		return fmt.Errorf("cpu wasn't expected; expected=%v got=%v", resource.NewScaledQuantity(int64(cpuMilli), resource.Milli), actualResources.Cpu)
+	}
+
+	if resource.NewDecimalQuantity(*inf.NewDec(int64(memMilli), inf.Scale(resource.Milli)), resource.DecimalSI).Equal(actualResources.Memory) {
+		return fmt.Errorf("memory wasn't expected; expected=%v got=%v", resource.NewScaledQuantity(int64(memMilli), resource.Milli), actualResources.Memory)
+	}
+
+	return nil
 }
