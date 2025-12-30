9436f9998f65c860a8b4863dd6d6b373cb306ae6
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2025-12-30 18:52:53 +0100

    savepoint next one

--- tests/e2e/tests/installation/installation_test.go
+++ tests/e2e/tests/installation/installation_test.go
@@ -9,11 +9,14 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"gopkg.in/inf.v0"
+	v2 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 
 	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/client"
 	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/httpbin"
@@ -111,8 +114,8 @@ func TestInstallation(t *testing.T) {
 		}
 	})
 	t.Run("Installation of Istio module with custom values", func(t *testing.T) {
-		//c, err := client.ResourcesClient(t)
-		//require.NoError(t, err)
+		c, err := client.ResourcesClient(t)
+		require.NoError(t, err)
 
 		require.NoError(
 			t,
@@ -148,6 +151,100 @@ func TestInstallation(t *testing.T) {
 		err = crdLister.EnsureCRDsArePresent(t.Context())
 		require.NoError(t, err)
 
+		istioNs := v1.Namespace{}
+		err = c.Get(t.Context(), "istio-system", "", &istioNs)
+		require.NoError(t, err)
+		_, ok := istioNs.Annotations["istios.operator.kyma-project.io/managed-by-disclaimer"]
+		require.True(t, ok, "istio-system namespace is not labeled with istios.operator.kyma-project.io/managed-by-disclaimer")
+		_, ok = istioNs.Labels["namespaces.warden.kyma-project.io/validate"]
+		require.True(t, ok, "istio-system namespace is not labeled with namespaces.warden.kyma-project.io/validate=true")
+
+		// istiod is ready
+		istiodDeployment := &v2.Deployment{}
+		err = c.Get(t.Context(), "istiod", "istio-system", istiodDeployment)
+		require.NoError(t, err)
+		err = wait.For(conditions.New(c).DeploymentConditionMatch(istiodDeployment, v2.DeploymentAvailable, v1.ConditionTrue), wait.WithContext(t.Context()))
+
+		// istio-ingressgateway is ready
+		ingressDeployment := &v2.Deployment{}
+		err = c.Get(t.Context(), "istio-ingressgateway", "istio-system", ingressDeployment)
+		require.NoError(t, err)
+		err = wait.For(conditions.New(c).DeploymentConditionMatch(ingressDeployment, v2.DeploymentAvailable, v1.ConditionTrue), wait.WithContext(t.Context()))
+		require.NoError(t, err)
+
+		// istio-egressgateway is ready
+		egressDeployment := &v2.Deployment{}
+		err = c.Get(t.Context(), "istio-egressgateway", "istio-system", egressDeployment)
+		require.NoError(t, err)
+		err = wait.For(conditions.New(c).DeploymentConditionMatch(egressDeployment, v2.DeploymentAvailable, v1.ConditionTrue), wait.WithContext(t.Context()))
+		require.NoError(t, err)
+
+		// istio-cni-node is ready
+		cniDaemonSet := &v2.DaemonSet{}
+		err = c.Get(t.Context(), "istio-cni-node", "istio-system", cniDaemonSet)
+		require.NoError(t, err)
+		err = wait.For(conditions.New(c).DaemonSetReady(cniDaemonSet), wait.WithContext(t.Context()))
+		require.NoError(t, err)
+
+		// ensure pilot limits and requests
+		istiodPodList := &v1.PodList{}
+		err = c.List(t.Context(), istiodPodList, resources.WithLabelSelector("app=istiod"))
+		require.NoError(t, err)
+		for _, pod := range istiodPodList.Items {
+			istiod := pod.Spec.Containers[0]
+			require.Equal(t, "discovery", istiod.Name)
+			err = assertResources(resourceStruct{
+				Cpu:    *istiod.Resources.Requests.Cpu(),
+				Memory: *istiod.Resources.Requests.Memory(),
+			}, "15m", "200Mi")
+			require.NoError(t, err)
+
+			err = assertResources(resourceStruct{
+				Cpu:    *istiod.Resources.Limits.Cpu(),
+				Memory: *istiod.Resources.Limits.Memory(),
+			}, "1200m", "1200Mi")
+			require.NoError(t, err)
+		}
+
+		// ensure ingressgateway limits and requests
+		ingressPodList := &v1.PodList{}
+		err = c.List(t.Context(), ingressPodList, resources.WithLabelSelector("app=istio-ingressgateway"))
+		require.NoError(t, err)
+		for _, pod := range ingressPodList.Items {
+			ingress := pod.Spec.Containers[0]
+			require.Equal(t, "istio-proxy", ingress.Name)
+			err = assertResources(resourceStruct{
+				Cpu:    *ingress.Resources.Requests.Cpu(),
+				Memory: *ingress.Resources.Requests.Memory(),
+			}, "80m", "200Mi")
+			require.NoError(t, err)
+
+			err = assertResources(resourceStruct{
+				Cpu:    *ingress.Resources.Limits.Cpu(),
+				Memory: *ingress.Resources.Limits.Memory(),
+			}, "1500m", "1200Mi")
+			require.NoError(t, err)
+		}
+
+		// ensure egressgateway limits and requests
+		egressPodList := &v1.PodList{}
+		err = c.List(t.Context(), egressPodList, resources.WithLabelSelector("app=istio-egressgateway"))
+		require.NoError(t, err)
+		for _, pod := range egressPodList.Items {
+			egress := pod.Spec.Containers[0]
+			require.Equal(t, "istio-proxy", egress.Name)
+			err = assertResources(resourceStruct{
+				Cpu:    *egress.Resources.Requests.Cpu(),
+				Memory: *egress.Resources.Requests.Memory(),
+			}, "70m", "190Mi")
+			require.NoError(t, err)
+
+			err = assertResources(resourceStruct{
+				Cpu:    *egress.Resources.Limits.Cpu(),
+				Memory: *egress.Resources.Limits.Memory(),
+			}, "1400m", "1100Mi")
+			require.NoError(t, err)
+		}
 	})
 }
 
